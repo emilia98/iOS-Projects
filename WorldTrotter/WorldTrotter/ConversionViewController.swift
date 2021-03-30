c15581c422fbe1f1593663089f7a61acc444741a
emilia98 <emilia_nedialkova@abv.bg>
2021-03-30 19:31:21 +0300

    add logic for converting in ConversionViewController

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -7,38 +7,76 @@
 
 import UIKit
 
-class ConversionViewController: UIViewController {
-    // var gradient = CAGradientLayer()
-    var bgColors: [UIColor] = [
-        UIColor.purple, UIColor.blue, UIColor.green,
-        UIColor.yellow, UIColor.red
-    ]
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        }
+        return nil
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print("ConversionViewController loaded its view")
-        
-        /*
-        gradient.colors = [
-            UIColor.purple.cgColor,
-            UIColor.blue.cgColor, UIColor.green.cgColor,
-            UIColor.yellow.cgColor, UIColor.red.cgColor
-        ]
-        
-        view.layer.insertSublayer(gradient, at: 0)
-        */
+        updateCelsiusLabel()
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        
-        // gradient.frame = view.bounds
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        view.backgroundColor = bgColors.randomElement()
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField)
+    {
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        
+        if existingTextHasDecimalSeparator != nil,
+           replacementTextHasDecimalSeparator != nil {
+            return false
+        }
+        return true
     }
 }
 
